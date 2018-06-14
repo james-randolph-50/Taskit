9d08d7068066258581daeea8a550e6cfe886d091
James <james.randolph2016@gmail.com>
2018-06-14 18:46:51 -0400

    Added user has task method in project shelper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,6 +4,10 @@ module ProjectsHelper
         project.tasks.any?{|task| task.completion_status.nil?}
     end
 
+    def user_has_task?(project)
+        current_user.tasks.any?{|task| task.project_id == project.id}
+    end
+
     def claimed_project_notice
     end
 end
