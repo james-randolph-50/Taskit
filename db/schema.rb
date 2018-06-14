cc0ecb650e0ceb18c243b85e2153b92cace34028
James <james.randolph2016@gmail.com>
2018-06-13 20:48:29 -0400

    added name to schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180613235153) do
 
   create_table "users", force: :cascade do |t|
     t.integer "team_id"
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
