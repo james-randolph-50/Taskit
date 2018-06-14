a52386458d0982d2adeb7f71e46d502943ec7dd2
James <james.randolph2016@gmail.com>
2018-06-13 20:16:12 -0400

    generated controller for users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static#index'
-  devise_for :users
+  devise_for :users #, controllers: {
+    #sessions: 'users/sessions'
+ # }
   resources :teams
   #root to: "home#index"
 end
