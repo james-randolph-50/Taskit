4d5bc3276b0ebbe7e366b6e85342db6485584863
James <james.randolph2016@gmail.com>
2018-06-13 21:37:31 -0400

    Generated resources for tasks and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :tasks
   root 'static#index', as: 'root'
   devise_for :users #, controllers: {
     #sessions: 'users/sessions'
