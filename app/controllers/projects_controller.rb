48561c0da0b0d1efde6cb2cd12101076a9871a5e
James <james.randolph2016@gmail.com>
2018-06-14 18:26:48 -0400

    can makes tasks in the project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
 
     def show
         @project = Project.find(params[:id])
+        @task = @project.tasks.build
     end
 
     def edit
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
 class TasksController < ApplicationController
+
+    def create
+        t = Task.new(task_params)
+    end
+
+    def task_params
+        params.require(:task).permit(:user_id, :chore_id)
+    end
 end
