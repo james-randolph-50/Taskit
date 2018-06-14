adc67f26a2a7bcaae740bc094f10f1b539532c91
James <james.randolph2016@gmail.com>
2018-06-14 17:31:35 -0400

    moved method from application helper to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_team
+
+  def current_team
+    current_user.team
+end
 
  # def require_login
   #  redirect_to root_path unless session[:user_id]
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,6 @@
 class ProjectsController < ApplicationController
+
+    def new
+        @project = current_team.projects.build
+    end
 end
