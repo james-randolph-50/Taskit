d1028bdb03ba8bddb29900aaf115f690a75493bc
James <james.randolph2016@gmail.com>
2018-06-13 20:01:25 -0400

    relationships of users to team; plus team_id

--- db/migrate/20180613234358_create_teams.rb
+++ db/migrate/20180613234358_create_teams.rb
@@ -1,8 +1,10 @@
 class CreateTeams < ActiveRecord::Migration[5.1]
   def change
     create_table :teams do |t|
+      t.string :name
+      t.string :password_digest
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
