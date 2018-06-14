95567978067acdfd3e0bc1fd42059d2cd5d2b138
James <james.randolph2016@gmail.com>
2018-06-14 19:56:20 -0400

    routes and non-user sign up copy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: ''
+  devise_for :users, path: '', path_names: { sign_up: 'users/sign_up'}
 
   root 'static#index', as: 'root'
 
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :tasks
  
   resources :users, only: [:show]
-  resources :teams
+  resources :teams, except: [:new]
+
+  get 'teams/sign_up' => 'teams#new', as: 'new_team'
 
   get 'teams/:id/users' => 'teams#users_index'
  
