502ecc58a7a2053b6760e1b18b206aef26266cdc
James <james.randolph2016@gmail.com>
2018-06-13 21:04:53 -0400

    added login reuired

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_login
+    redirect_to root_path unless session[:user_id]
+  end
+  
 end
