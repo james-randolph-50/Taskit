52e24e751855ae70fd16b206df8501ad9cb03aa6
James <james.randolph2016@gmail.com>
2018-06-14 17:41:15 -0400

    adding methods to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,17 @@ class ProjectsController < ApplicationController
     def new
         @project = current_team.projects.build
     end
+
+    def create
+        @project = Project.create(project_params)
+        redirect_to team_path(current_team)
+    end
+
+    def edit
+    end
+
+    private
+
+    def project_params
+    end
 end
