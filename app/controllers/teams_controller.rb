3d108fe982251f74184bd92a30866b95efb275f6
James <james.randolph2016@gmail.com>
2018-06-13 20:49:57 -0400

    new and create methods in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,14 @@
 class TeamsController < ApplicationController
+
+    def new
+        @team = Team.new
+    end
+
+    def create
+        @team = Team.create(team_params)
+    end
+
+    private
+
+    
 end
