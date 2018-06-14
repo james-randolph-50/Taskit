a6ab0b091b1f9329b88b6314797f44ab8ee61f87
James <james.randolph2016@gmail.com>
2018-06-14 18:33:15 -0400

    added completion status to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614013701) do
+ActiveRecord::Schema.define(version: 20180614223104) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
