164bf47a2877601ea0d1da6bf4bcd6c0528d0829
James <james.randolph2016@gmail.com>
2018-06-14 20:02:01 -0400

    routes on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_up: 'users/sign_up'}
 
+  get 'teams/sign_up' => 'teams#new', as: 'new_team'
+  get 'teams/:id/users' => 'teams#users_index'
+
   root 'static#index', as: 'root'
 
   resources :projects
@@ -9,9 +12,5 @@ Rails.application.routes.draw do
  
   resources :users, only: [:show]
   resources :teams, except: [:new]
-
-  get 'teams/sign_up' => 'teams#new', as: 'new_team'
-
-  get 'teams/:id/users' => 'teams#users_index'
  
 end
